2a74d8ddba86369b12ee740374c1f1d612efb635
NATNAEL BIRHANU <negusnati@gmail.com>
2022-01-30 08:07:26 -0800

    added new Record_Office Class

---
+++ src/main/java/record/Record_Office.java
@@ -0,0 +1,27 @@
+package record;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Record_Office extends Application {
+
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Record.class.getResource("record.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Record !");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
