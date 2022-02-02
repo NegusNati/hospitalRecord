97e69e4713515e4770f4975344261f8a376da65d
NegusNati <negusnati@gmail.com>
2022-02-02 01:08:12 -0800

    added buttons to home page

--- src/main/java/com/example/hospitalrecord/HelloController.java
+++ src/main/java/com/example/hospitalrecord/HelloController.java
@@ -1,12 +1,69 @@
 package com.example.hospitalrecord;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
 
     @FXML
+    private Button btn_admin;
+
+    @FXML
+    private Button btn_doc;
+
+    @FXML
+    private Button btn_lab;
+
+    @FXML
+    private Button btn_nurse;
+
+    @FXML
+    private Button btn_patient;
+
+    @FXML
+    private Button btn_pharma;
+
+    @FXML
+    private Button btn_rec;
+
+    @FXML
     private Label lable1;
 
+    @FXML
+    void admin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lab(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nurse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void patient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pharma(ActionEvent event) {
+
+    }
+
+    @FXML
+    void record(ActionEvent event) {
+
+    }
 
 }
