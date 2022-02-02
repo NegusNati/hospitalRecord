4858b0787cb1f7765d3af0aed4acc71b44060a9c
NegusNati <negusnati@gmail.com>
2022-02-01 23:32:03 -0800

    added styles.css and configured bugs

--- src/main/java/com/example/hospitalrecord/HospitalRecord.java
+++ src/main/java/com/example/hospitalrecord/HospitalRecord.java
@@ -11,7 +11,7 @@ public class HospitalRecord extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HospitalRecord.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hospital !");
         stage.setScene(scene);
         stage.show();
